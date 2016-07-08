93866130cfe68a66611a2e3c4cc2e2be5d1a8d1f
JiajunChen <jiajun.chen@mail.mcgill.ca>
2016-07-07 21:04:12 -0400

    check

--- src/GUI/CardImage.java
+++ src/GUI/CardImage.java
@@ -1,9 +1,4 @@
 package GUI;
-
-
-
-
-
 import java.util.HashMap;
 
 import javafx.scene.image.Image;
@@ -18,7 +13,6 @@ import resource.Card.Suit;
  */
 public final class CardImage 
 {
-	
 	private final static String[] prefix = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
 	private final static String[] sufix = {"clubs", "diamonds", "spades", "hearts"};
 	private final static int cardWidth = 80;
